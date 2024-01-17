919c39f018c19c712d2598726f1971ad4e9109d4
65160013 <65160013@go.buu.ac.th>
2024-01-17 19:25:54 +0700

    เพิ่มคำอธิบายเพิ่มเติมใน JavaScript

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded",function() {
     const todoList = document.getElementById("todo-list");
     const todoInput = document.getElementById("todo-input");
     const addButton = document.getElementById("add-button");
-
+    
+    // อาร์เรย์สำหรับเก็บรายการ Todo
     let todos = [];
 
     function addTodo() {
@@ -18,20 +19,24 @@ document.addEventListener("DOMContentLoaded",function() {
         }
     }
 
+    // ลบรายการ Todo
     function deleteTodo(index) {
         todos.splice(index,1);
         renderTodoList();
     }
 
+    // ตรวจสอบ or ยกเลิกการเสร็จสิ้นรายการ Todo
     function toggleComplete(index) {
         todos[index].completed = !todos[index].completed;
         renderTodoList();
     }
 
+    // แสดงรายการ Todo บนหน้าเว็บ
     function renderTodoList() {
         console.log(todos);
         todoList.innerHTML = "";
 
+        // สร้างแสดงรายการ Todo โดยมีปุ่ม ลบ เสร็จ ยกเลิก สำหรับแต่ละรายการที่เพิ่มเข้ามาใหม่
         for(let i = 0; i < todos.length; i++) {
             const todoItem = todos[i];
             const listItem = document.createElement("li");
@@ -51,12 +56,16 @@ document.addEventListener("DOMContentLoaded",function() {
         }
     }
 
+    // การกดปุ่ม "เพิ่ม"
     addButton.addEventListener("click", addTodo);
 
+    // การกดปุ่ม Enter ใน input
     todoInput.addEventListener("keypress", function(event) {
         if(event.key === "Enter") {
             addTodo();
         }
     });
+
+    // แสดงรายการ Todo คร้ังแรก
     renderTodoList();
 });
